210d4a4427716a0f807348bd5657464d584a2eaf
Ricardo Boccoli Gallego <ricardo.gallego93@gmail.com>
2016-07-22 15:02:34 -0300

    Removed old scripts

--- scripts/edge-detector.R
+++
@@ -1,8 +0,0 @@
-#EDGE-DETECTOR
-
-args <- commandArgs(trailingOnly=TRUE)
-if (length(args) != 1)
-    stop("Usage: Rscript edge-detector.R VARIABLE")
-
-variable <- args[1]
-input <- read.table("stdin", header=TRUE)
